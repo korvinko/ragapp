1b36fa35704dd5b23e070cc61409d43a79292bbd
korvinko <dzmitry.kasenka@gmail.com>
2024-06-30 23:59:55 +0300

    Init

---
+++ helpers.py
@@ -0,0 +1,25 @@
+# import
+from langchain_community.vectorstores import LanceDB
+from langchain_community.embeddings import OllamaEmbeddings
+import lancedb
+
+def getEmbeddings():
+    # create the open-source embedding function
+    return OllamaEmbeddings(base_url="http://localhost:11434", model="rjmalagon/gte-qwen2-7b-instruct-embed-f16")
+
+def getVectorStore():
+    # Connect to local LanceDB
+    db_path = "/tmp/lancedb_1"  # Adjust the path as necessary for your setup
+    db = lancedb.connect(uri=db_path)
+
+    # create the open-source embedding function
+    embeddings = getEmbeddings()
+
+    # Initialize LanceDB vector store
+    vc = LanceDB(
+        uri=db_path,
+        embedding=embeddings,
+        table_name='langchain_test'
+    )
+
+    return vc
